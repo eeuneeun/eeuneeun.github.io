1f14f15bf76eef39f708dbe67a6c3b371ddd70ba
eeuneeun <sacroo89@gmail.com>
2018-01-09 15:14:55 +0900

    120109

---
+++ study/node/chapter-01/server/config/config.js
@@ -0,0 +1,5 @@
+//DB url
+module.exports={
+    //locall machine connect Mongo DB
+    'url' : 'mongodb://localhost/mvc-app'
+}
--- study/node/chapter-01/server/routes/index.js
+++ study/node/chapter-01/server/routes/index.js
@@ -6,4 +6,26 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: '헬로 월드!?' });
 });
 
+/* GET 메서드용 로그인 페이지 */
+router.get('/login', function(req, res, next) {
+  res.render('login', { title: 'Login Page' , massage : req.flash('loginMessage') });
+});
+
+/* GET 메서드용 가입 페이지 */
+router.get('/signup', function(req, res) {
+  res.render('signup', { title: 'Signup Page' , massage : req.flash('signupMessage') });
+});
+
+/* GET 메서드용 프로필 페이지 */
+router.get('/profile', function(req, res, next) {
+  res.render('profile', { 
+    title : 'Profile Page' , 
+    user : req.user,
+    avatar : gravatar.url(req.user.email , {s : '100', r : 'x', d: 'retro'}, true)
+  });
+});
+
+
 module.exports = router;
+
+
